a198412d7dec7afdaf0648f2fa9d94ebfb0aea4b
James Penn <james@jamespenn.co.uk>
2014-05-31 16:09:20 +0100

    Extracted vector functions into their own file

--- js/dimension.js
+++ js/dimension.js
@@ -1,58 +1,3 @@
-var Vector = function (x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.arr = [x,y,z];
-    this.length = Math.sqrt(this.abs());
-};
-
-Vector.prototype = {
-    add: function (v) {
-        return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
-    },
-    invert: function () {
-        return new Vector(-this.x, -this.y, -this.z);
-    },
-    scale: function (s) {
-        return new Vector(this.x * s, this.y * s, this.z * s);
-    },
-    subtract: function (v) {
-        return this.add(v.invert());
-    },
-    dot: function (v) {
-        return this.x * v.x + this.y * v.y + this.z * v.z;
-    },
-    abs: function() {
-        return this.dot(this);
-    },
-    normalise: function(){
-        var len = this.length;
-        return this.scale(1/this.length);
-    },
-    project: function(v) {
-        // project this vector along v
-        return this.scale(this.dot(v) / this.abs());
-    },
-    dist: function(to) { // distance to another vector
-        return this.subtract(to).length
-    }
-};
-
-function gram_schmidt (vectors, normalise) {
-    var normalise = normalise || false;
-    var orthog_vectors = [];
-    vectors.forEach(function (v) {
-        var p = orthog_vectors.map(function(u) { return u.project(v); });
-        var u = v.subtract(p.reduce(function(u,v) { return u.add(v); }, new Vector(0,0,0)));
-        orthog_vectors.push(u);
-    });
-    if (normalise) {
-        return orthog_vectors.map(function(v) { return v.normalise(); });
-    } else {
-        return orthog_vectors;
-    }
-}
-
 function make_lorenz(sigma, r, b) {
     return function lorenz(pos) {
         var xdot = sigma * (pos.y - pos.x),
@@ -73,14 +18,6 @@ function make_lorenz_jacobian(sigma, r, b) {
     }
 }
 
-function vectorRK4(x, h, fn) {
-    // perform a step of rk4 using the given step size and function
-    var k1 = fn(x).scale(h),
-        k2 = fn(x.add(k1.scale(0.5))).scale(h),
-        k3 = fn(x.add(k2.scale(0.5))).scale(h),
-        k4 = fn(x.add(k3)).scale(h);
-    return x.add((k1.add(k2.scale(2)).add(k3.scale(2)).add(k4)).scale(1 / 6));
-}
 
 
 
